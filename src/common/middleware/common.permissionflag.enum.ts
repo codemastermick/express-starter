6b4658ca99d110f693692f4ed6a4918b12c31706
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-01-23 15:07:37 -0500

    chore: update flags and add comment to top of file

--- src/common/middleware/common.permissionflag.enum.ts
+++ src/common/middleware/common.permissionflag.enum.ts
@@ -1,8 +1,16 @@
+/* This is a bitwise flag enum, meaning each flag must be
+ * set up as the previous flag's number, multiplied by two.
+ * The ALL_PERMISSIONS flag is a sudo level flag that should
+ * never be assigned to normal users in production as it will
+ * allow access to ALL features of the app.
+ */
 //TODO change these flags to be relevant to your project
 export enum PermissionFlag {
   FREE_PERMISSION = 1,
-  PAID_PERMISSION = 2,
-  ANOTHER_PAID_PERMISSION = 4,
-  ADMIN_PERMISSION = 8,
+  APP_PERMISSION_A = 2,
+  APP_PERMISSION_B = 4,
+  PAID_FEATURE_A = 8,
+  PAID_FEATURE_B = 16,
+  ADMIN_PERMISSION = 32,
   ALL_PERMISSIONS = 2147483647,
 }
