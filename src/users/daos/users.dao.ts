df34adbf6d44d51066b9b87eebd2448b27b97053
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-01-23 13:48:33 -0500

    chore: introduce enum for permission flags

--- src/users/daos/users.dao.ts
+++ src/users/daos/users.dao.ts
@@ -4,6 +4,7 @@ import { PutUserDto } from "../dto/put.user.dto";
 import shortid from "shortid";
 import debug from "debug";
 import mongooseService from "../../common/services/mongoose.service";
+import { PermissionFlag } from "../../common/middleware/common.permissionflag.enum";
 
 const log: debug.IDebugger = debug("app:in-memory-dao");
 
@@ -31,7 +32,7 @@ class UsersDao {
     const user = new this.User({
       _id: userId,
       ...userFields,
-      permissionFlags: 1,
+      permissionFlags: PermissionFlag.FREE_PERMISSION,
     });
     await user.save();
     return userId;
