fa43d209240e634bf42d21b8681c3577718d6289
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-01-24 11:28:24 -0500

    style: lint and format

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,8 +1,8 @@
-import app from "../src/app";
-import supertest from "supertest";
-import { expect } from "chai";
+import app from '../src/app';
+import supertest from 'supertest';
+import { expect } from 'chai';
 
-describe("Index Test", function () {
+describe('Index Test', function () {
   let request: supertest.SuperAgentTest;
   before(function () {
     request = supertest.agent(app);
@@ -13,8 +13,8 @@ describe("Index Test", function () {
     done();
   });
 
-  it("should allow a GET to /", async function () {
-    const res = await request.get("/").send();
+  it('should allow a GET to /', async function () {
+    const res = await request.get('/').send();
     expect(res.status).to.equal(200);
     expect(res.text).equals(
       `Server running at http://localhost:${process.env.PORT}`
