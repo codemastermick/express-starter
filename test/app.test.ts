a9581d825b7b50e067838e246ad7cf29f0c69e07
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-01-23 19:01:08 -0500

    test: add test suite for app.ts

---
+++ test/app.test.ts
@@ -0,0 +1,23 @@
+import app from "../src/app";
+import supertest from "supertest";
+import { expect } from "chai";
+
+describe("Index Test", function () {
+  let request: supertest.SuperAgentTest;
+  before(function () {
+    request = supertest.agent(app);
+  });
+  after(function (done) {
+    // shut down the Express.js server
+    app.close();
+    done();
+  });
+
+  it("should allow a GET to /", async function () {
+    const res = await request.get("/").send();
+    expect(res.status).to.equal(200);
+    expect(res.text).equals(
+      `Server running at http://localhost:${process.env.PORT}`
+    );
+  });
+});
