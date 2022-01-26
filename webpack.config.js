d7c43d68bcbe03c54ec6b43f92f6648fd65e9b04
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-01-26 11:24:42 -0500

    chore: commit webpack config

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const {
+  NODE_ENV = 'production',
+} = process.env;
+
+module.exports = {
+  entry: {
+    index: './src/app.ts',
+    auth: './src/auth/auth.routes.config.ts',
+    users: './src/users/users.routes.config.ts'
+  },
+  // entry: './src/app.ts',
+  mode: NODE_ENV,
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  externalsPresets: { node: true },
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  optimization: {
+    usedExports: true
+  },
+  watch: NODE_ENV === 'development'
+};
