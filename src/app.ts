8ed2d66e53f9828f49691e62b3788d900d130165
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-01-28 14:11:29 -0500

    chore: remove unused code

--- src/app.ts
+++ src/app.ts
@@ -14,10 +14,7 @@ import { UsersRoutes } from './users/users.routes.config';
 import { AuthRoutes } from './auth/auth.routes.config';
 import debug from 'debug';
 import helmet from 'helmet';
-import { webpack } from 'webpack';
-import config from '../webpack.config.js';
 
-const compiler = webpack(config);
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
 const port = process.env.PORT;
