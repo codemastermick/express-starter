2b1c06b3b9b870d0f6188e24cb9bd47f82d3d29e
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-01-30 14:04:19 -0500

    chore: move webpack configs

--- configs/webpack.common.js
+++ configs/webpack.common.js
@@ -4,9 +4,9 @@ const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
   entry: {
-    index: './src/app.ts',
-    auth: './src/auth/auth.routes.config.ts',
-    users: './src/users/users.routes.config.ts'
+    index: path.resolve('src/app.ts'),
+    auth: path.resolve('src/auth/auth.routes.config.ts'),
+    users: path.resolve('src/users/users.routes.config.ts')
   },
   externalsPresets: { node: true },
   externals: [nodeExternals()],
@@ -28,9 +28,9 @@ module.exports = {
   target: 'node',
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist/'),
+    path: path.resolve('dist'),
     clean: true,
-    publicPath: './dist/'
+    publicPath: 'dist/'
   },
   optimization: {
     usedExports: true
