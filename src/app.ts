d90490f838fef081cdc69f9fc13c270078bfcd40
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-01-30 11:12:54 -0500

    feat: add more powerful logging service + middleware

--- src/app.ts
+++ src/app.ts
@@ -5,49 +5,31 @@ if (dotenvResult.error) {
 }
 import express from 'express';
 import * as http from 'http';
-
-import * as winston from 'winston';
-import * as expressWinston from 'express-winston';
 import cors from 'cors';
 import { CommonRoutesConfig } from './common/common.routes.config';
 import { UsersRoutes } from './users/users.routes.config';
 import { AuthRoutes } from './auth/auth.routes.config';
 import debug from 'debug';
 import helmet from 'helmet';
+import morganMiddleware from './common/middleware/morgan.middleware';
+import Logger from './common/services/logger.service';
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
 const port = process.env.PORT;
 const routes: Array<CommonRoutesConfig> = [];
 const debugLog: debug.IDebugger = debug('app');
+const logger: Logger = new Logger('express-starter');
 
 // here we are adding middleware to parse all incoming requests as JSON
 app.use(express.json());
 
 // here we are adding middleware to allow cross-origin requests
 app.use(cors());
+// the helmet middleware is a collection of settings for http server security
 app.use(helmet());
-
-// here we are preparing the expressWinston logging middleware configuration,
-// which will automatically log all HTTP requests handled by Express.js
-const loggerOptions: expressWinston.LoggerOptions = {
-  transports: [new winston.transports.Console()],
-  format: winston.format.combine(
-    winston.format.json(),
-    winston.format.prettyPrint(),
-    winston.format.colorize({ all: true })
-  ),
-};
-
-if (!process.env.DEBUG) {
-  loggerOptions.meta = false; // when not debugging, log requests as one-liners
-  if (typeof global.it === 'function') {
-    loggerOptions.level = 'http'; // for non-debug test runs, squelch entirely
-  }
-}
-
-// initialize the logger with the above configuration
-app.use(expressWinston.logger(loggerOptions));
+// this is our custom logging middleware
+app.use(morganMiddleware);
 
 // here we are adding the UserRoutes to our array,
 // after sending the Express.js application object to have the routes added to our app!
@@ -65,9 +47,7 @@ export default server;
 
 server.listen(port, () => {
   routes.forEach((route: CommonRoutesConfig) => {
-    debugLog(`Routes configured for ${route.getName()}`);
+    logger.debug(`Routes configured for ${route.getName()}`);
   });
-  // our only exception to avoiding console.log(), because we
-  // always want to know when the server is done starting up
-  console.log(runningMessage);
+  logger.debug(runningMessage);
 });
---
+++ src/common/services/logger.service.ts
@@ -0,0 +1,97 @@
+import winston from 'winston';
+import debug from 'debug';
+import {
+  accessLog,
+  allLogs,
+  logLevelColours,
+  errorLog,
+  logLevelNames,
+  timestampFormat,
+} from '../config/logger.config';
+
+const { combine, timestamp, label } = winston.format;
+
+const level = () => {
+  const env = (process.env.NODE_ENV as string) || 'development';
+  const isDevelopment = env === 'development';
+  return isDevelopment ? 'debug' : 'warn';
+};
+
+const format = winston.format.combine(
+  winston.format.timestamp({ format: timestampFormat }),
+  winston.format.colorize({ all: true, colors: logLevelColours }),
+  winston.format.printf(
+    (info) => `${info.timestamp} ${info.level}: ${info.message}`
+  )
+);
+
+const fileFormat = winston.format.combine(
+  winston.format.timestamp({ format: timestampFormat }),
+  winston.format.printf(
+    (info) => `${info.timestamp} ${info.level}: ${info.message}`
+  )
+);
+
+const transports = [
+  new winston.transports.Console({ format }),
+  new winston.transports.File({
+    filename: errorLog,
+    level: 'error',
+    format: fileFormat,
+  }),
+  new winston.transports.File({
+    filename: allLogs,
+    format: fileFormat,
+  }),
+];
+
+const requestTransport = [
+  new winston.transports.File({
+    filename: accessLog,
+    format: fileFormat,
+  }),
+];
+
+export default class Logger {
+  private log: winston.Logger;
+  private requestLogger: winston.Logger;
+  private debugLog: debug.IDebugger;
+
+  constructor(appName: string) {
+    this.log = winston.createLogger({
+      level: level(),
+      levels: logLevelNames,
+      format: combine(label({ label: appName, message: true }), timestamp()),
+      transports,
+    });
+    this.requestLogger = winston.createLogger({
+      level: 'http',
+      levels: { http: 0 },
+      format: combine(label({ label: appName, message: true }), timestamp()),
+      transports: requestTransport,
+    });
+    this.debugLog = debug(appName);
+  }
+
+  error(message: string) {
+    this.log.error(message);
+  }
+
+  warn(message: string) {
+    this.log.warn(message);
+  }
+
+  info(message: string) {
+    this.log.info(message);
+  }
+
+  http(message: string) {
+    this.log.http(message);
+    this.requestLogger.http(message);
+  }
+
+  debug(message: string) {
+    this.log.debug(message);
+    this.debugLog(message);
+  }
+}
